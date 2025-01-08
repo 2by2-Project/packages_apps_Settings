2by2-Project/android_packages_apps_Settings

Settings: Integrate 2byTricks on Top level settings page * Show clickable 2by2 banner on Top level settings page. * Used the crDroid, Evolution X, ProjectBlaze implementation as a reference.
Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
Co-authored-by: Anierin Bliss <anierin@evolution-x.org>
Co-authored-by: Joey <joey@evolution-x.org>
Co-authored-by: Aditya Pratap Singh <shivamatiet2001@gmail.com>

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -204,6 +204,8 @@ import com.android.settings.wifi.p2p.WifiP2pSettings;
 import com.android.settings.wifi.savedaccesspoints2.SavedAccessPointsWifiSettings2;
 import com.android.settings.wifi.tether.WifiTetherSettings;
 
+import jp.project2by2.settings.CustomSettings;
+
 public class SettingsGateway {
 
     /**
@@ -396,6 +398,8 @@ public class SettingsGateway {
             CellularSecuritySettingsFragment.class.getName(),
             AccessibilityHearingAidsFragment.class.getName(),
             HearingDevicePairingFragment.class.getName(),
+            // 2by2 Project additions
+            CustomSettings.class.getName(),
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -442,5 +446,7 @@ public class SettingsGateway {
             UserBackupSettingsActivity.class.getName(),
             Settings.MemtagPageActivity.class.getName(),
             Settings.NavigationModeSettingsActivity.class.getName(),
+            // 2by2 Project additions
+            Settings.CustomSettingsActivity.class.getName(),
     };
 }

2by2-Project/packages_apps_Settings

Change-Id: I0e2b7384e5ba97e6916e2f2da70bf545fbda7c36

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -212,6 +212,8 @@ import com.android.settings.wifi.tether.WifiTetherSettings;
 import com.android.settings.livedisplay.LiveDisplaySettings;
 import ink.kscope.settings.wifi.tether.WifiTetherClientManager;
 
+import jp.project2by2.settings.CustomSettings;
+
 public class SettingsGateway {
 
     /**
@@ -410,7 +412,10 @@ public class SettingsGateway {
             ZenModeFragment.class.getName(),
             LiveDisplaySettings.class.getName(),
             WifiTetherClientManager.class.getName(),
-            WirelessDebuggingFragment.class.getName()
+            WirelessDebuggingFragment.class.getName(),
+
+            // 2by2 Project additions
+            CustomSettings.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -458,5 +463,8 @@ public class SettingsGateway {
             Settings.MemtagPageActivity.class.getName(),
             Settings.NavigationModeSettingsActivity.class.getName(),
             Settings.WirelessDebuggingActivity.class.getName(),
+
+            // 2by2 Project additions
+            Settings.CustomSettingsActivity.class.getName(),
     };
 }
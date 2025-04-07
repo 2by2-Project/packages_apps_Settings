221c9f278fe2bd6904c435bfc35d1d7dc7d5cc10
kailua <kailua@project2by2.jp>
2025-04-07 19:32:05 +0900

    Settings: Redesign settings top page * Reorder top_level_settings and put divider * Enable white/black theme * 1dp stroke on top search bar

--- src/com/android/settings/homepage/TopLevelSettings.java
+++ src/com/android/settings/homepage/TopLevelSettings.java
@@ -88,7 +88,7 @@ public class TopLevelSettings extends DashboardFragment implements SplitLayoutLi
 
     @Override
     protected int getPreferenceScreenResId() {
-        return Flags.homepageRevamp() ? R.xml.top_level_settings_v2 : R.xml.top_level_settings;
+        return R.xml.top_level_settings_customized;
     }
 
     @Override
@@ -394,9 +394,7 @@ public class TopLevelSettings extends DashboardFragment implements SplitLayoutLi
 
     public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
             new BaseSearchIndexProvider(
-                    Flags.homepageRevamp()
-                            ? R.xml.top_level_settings_v2
-                            : R.xml.top_level_settings) {
+                    R.xml.top_level_settings_customized) {
 
                 @Override
                 protected boolean isPageSearchEnabled(Context context) {

ff5bed31a8e9c8829f085e4a55fe31d9f942560b
kailua <kailua@project2by2.jp>
2025-07-25 01:57:52 +0900

    Revert "Settings: Allow user to provide keybox data [3/3]"
    This reverts commit dc33a68bca765ea1761891cd4fcaeff01c685efd.

--- src/com/android/settings/system/SpecialSettings.java
+++ src/com/android/settings/system/SpecialSettings.java
@@ -5,12 +5,8 @@
 
 package com.android.settings.system;
 
-import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Toast;
 
 import com.android.internal.logging.nano.MetricsProto;
@@ -19,14 +15,10 @@ import com.android.settings.R;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settingslib.search.SearchIndexable;
 
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.preference.Preference;
 import androidx.preference.Preference.OnPreferenceChangeListener;
 
 import java.util.Arrays;
-
-import jp.project2by2.settings.preferences.KeyboxDataPreference;
 
 @SearchIndexable
 public class SpecialSettings extends DashboardFragment implements
@@ -36,36 +28,9 @@ public class SpecialSettings extends DashboardFragment implements
 
     private Toast mToast;
 
-    private static final String KEYBOX_DATA_KEY = "spoofing_keybox_data_setting";
-
-    private ActivityResultLauncher<Intent> mKeyboxFilePickerLauncher;
-    private KeyboxDataPreference mKeyboxDataPreference;
-
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
-
-        mKeyboxFilePickerLauncher = registerForActivityResult(
-        new ActivityResultContracts.StartActivityForResult(),
-            result -> {
-                if (result.getResultCode() == Activity.RESULT_OK && result.getData() != null) {
-                    Uri uri = result.getData().getData();
-                    Preference pref = findPreference(KEYBOX_DATA_KEY);
-                    if (pref instanceof KeyboxDataPreference) {
-                        ((KeyboxDataPreference) pref).handleFileSelected(uri);
-                    }
-                }
-            }
-        );
-    }
-
-    @Override
-    public void onViewCreated(View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        mKeyboxDataPreference = findPreference(KEYBOX_DATA_KEY);
-        if (mKeyboxDataPreference != null) {
-            mKeyboxDataPreference.setFilePickerLauncher(mKeyboxFilePickerLauncher);
-        }
     }
 
     @Override
